aa4531dceac5903d78f5efb230b03bf743def757
isx@sleekbook.linux <men_one89@hotmail.com>
2015-03-29 00:03:47 -0600

    add get_matrix_value and fill the data

--- main_linear_reg.cpp
+++ main_linear_reg.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//TODO change the fscanf to aply with all data_type types (float.. double ....)
 int** data_features;
 
 typedef int data_type;
@@ -8,12 +8,25 @@ struct data_set
 {
 	unsigned int number_of_data_features;
 	unsigned int number_of_training_set;
-	data_type* data_features;
-	data_type* data_result;
+	data_type* data_features_x;
+	data_type* data_result_y;
 };
 typedef struct data_set data_set;
 
-int load_data(char* file_name,data_set* data)
+unsigned int malloc_space_for_data_set(data_set* data)
+{
+	
+	data->data_features_x = (data_type*) malloc(data->number_of_training_set*data->number_of_data_features*sizeof(data_type));
+	data->data_result_y = (data_type*)  malloc(data->number_of_training_set*sizeof(data_type));
+}
+//return the value of row and column of a data_matrix
+data_type get_matrix_value(unsigned int fil,unsigned int col,unsigned int n_cols,data_type* data_matrix)
+{
+	//need to know column number
+	return *(data_matrix + (fil*n_cols +col ));
+}
+
+int load_data(const char* file_name,data_set* data)
 {
 	FILE* file = fopen(file_name,"r");
 	if(!file)
@@ -21,8 +34,30 @@ int load_data(char* file_name,data_set* data)
 		return 0;
 	}
 	fscanf(file,"%d %d",&data->number_of_data_features,&data->number_of_training_set);
-	//TODO Give a try to check if this function is working, just need to print the two readed values.
-	//TODO create a bucle to load all the training set.
+	//Give a try to check if this function is working, just need to print the two readed values.
+	printf("%d %d\n",data->number_of_data_features,data->number_of_training_set);
+	malloc_space_for_data_set(data);
+	
+	int idx = 0;
+	int n_feature = 0;
+	data_type aux_data;
+	
+	//Blucle to load the data set
+	for(idx=0;idx<data->number_of_training_set;idx++)
+	{
+		for(n_feature = 0;n_feature<data->number_of_data_features;n_feature++)
+		{
+			//Read and storage data in fetures vector
+			fscanf(file,"%d",&aux_data);
+			*(data->data_features_x +(idx * data->number_of_data_features + n_feature)) = aux_data;
+			printf("%d ",aux_data);
+			//Read and storage data in y vector
+			fscanf(file,"%d",&aux_data);
+			printf("%d\n",aux_data);
+			*(data->data_result_y + idx) = aux_data;
+		}
+	
+	}
 	fclose(file);
 	return 1;
 }
@@ -30,7 +65,9 @@ int load_data(char* file_name,data_set* data)
 
 int main(int argc, char** argv)
 {
+	data_set data;
 	printf("Hola mundo\n");
-	
+	load_data("data.txt",&data);
+	printf(".%d",get_matrix_value(9,0,1,data.data_features_x));
 	return 0;
 }
